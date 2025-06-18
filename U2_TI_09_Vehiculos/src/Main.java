b5df461c705450b4f10108065fe39b13333c83f2
zurira <20243ds036@utez.edu.mx>
2025-06-18 10:02:24 -0600

    Realice algunas correciones

--- U2_TI_09_Vehiculos/src/Main.java
+++ U2_TI_09_Vehiculos/src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //ZURI
         Garage garage =new Garage(4);
 
         Vehiculo coche1 = new Coche("2024FFSD", "DODGE", 4);
